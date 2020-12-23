c5c2417b46ef796c51568963ba51756b69a095ef
Dikshha2601 <66013703+Dikshha2601@users.noreply.github.com>
2020-12-23 14:45:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function preload() {
+  world_start = loadSound("world_start.wav");
+  setSprites();
+  MarioAnimation();
+}
+
+function setup() {
+  canvas = createCanvas(1240, 336);
+  canvas.parent("canvas");
+  instializeInSetup(mario);
+
+  video = createCapture(VIDEO);
+  video.size(800, 400);
+  video.parent("game_console");
+
+  poseNet = ml5.poseNet(video, function () {
+    console.log("Model Loaded!");
+  });
+  poseNet.on("pose", function (results) {
+    if (results.length > 0) {
+      console.log(results);
+      noseX = results[0].pose.nose.x;
+      noseY = results[0].pose.nose.y;
+    }
+  });
+}
+
+function draw() {
+  game();
+}
